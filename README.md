8ea915bc5da85276aa597eda8b3f48cd9d987aa1
ritwicksamanta <ritwick.samanta001@gmail.com>
2025-08-17 12:51:10 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+
+# [Blind 75](https://leetcode.com/problem-list/oizxjoit/)
+
+## Problem List
+| #  | Problem Name                                                                 | Difficulty | Solution Link |
+|----|------------------------------------------------------------------------------|------------|---------------|
+| 1  | [Two Sum](https://leetcode.com/problems/two-sum/)                            | Easy       | [Solution](./TwoSum.java) |
+
+
---
+++ TwoSum.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+class TwoSum{
+    public static void main(String[] args) {
+        int[] arr = new int[]{
+            1,2,3,4,5,6,7,-1,0,9,8,100
+        };
+        int target = -1;
+        System.out.println("Index of numbers that add upto "+target+" :"+ 
+        Arrays.toString(Solution.twoSum(arr, target)));
+    }
+    
+
+    static class Solution{
+        public static int[] twoSum(int[] nums, int target) {
+            Map<Integer,Integer> map = new HashMap<>();
+            for(int i = 0; i<nums.length; i++){
+                if(map.containsKey(target-nums[i]))
+                    return new int[]{
+                        i,map.get(target-nums[i])
+                    };
+                map.put(nums[i],i);
+            }
+            return new int[]{-1};
+        }
+    }
+}
